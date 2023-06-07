f62f4338a0c1489a4649e8eea4a888fed96cf3ed
larissa <127438009+pipocore@users.noreply.github.com>
2023-06-07 16:14:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 app.use(express.static(__dirname + '/public'));
-app.use(favicon(app.get('public'), 'favicon.ico'));
+app.use(favicon(__dirname + '/public' + 'favicon.ico'));
 app.set('trust proxy', 1)
 app.use(session({
     secret: process.env.SECRET,
